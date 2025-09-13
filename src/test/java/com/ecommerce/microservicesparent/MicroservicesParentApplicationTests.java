1028f8c77d1843338186e1f60ed12d075e539491
Clever Machaca <106214364+cleverjohann@users.noreply.github.com>
2025-09-12 22:37:56 -0500

    Add base configurations for microservices
    - Initialized Maven wrapper for all services.
    - Added `.gitignore` files with common IDE, build, and tool ignores.
    - Added `.gitattributes` for line ending normalization.
    - Created application setup for `api-gateway`, `eureka-server`, `inventory-service`, `order-service`, and `shared-lib` services with Spring Boot applications and test placeholders.

---
+++ src/test/java/com/ecommerce/microservicesparent/MicroservicesParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.microservicesparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
